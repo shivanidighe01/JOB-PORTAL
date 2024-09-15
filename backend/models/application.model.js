1a3eeac2ce87c92dabd261f10c05026a9d6c19f5
shivanidighe01 <shivanidighe2003@gmail.com>
2024-09-15 18:44:45 +0530

    user controller and model done

--- backend/models/application.model.js
+++ backend/models/application.model.js
@@ -13,6 +13,7 @@ const applicationSchema=new mongoose.Schema(
             type:mongoose.Schema.Types.ObjectId,
             ref:'User',
             required:true
+            
         },
         status:
         {
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema=mongoose.Schema(
     {
-        fullname:
+        fullame:
         {
             type:String,
             required:true
